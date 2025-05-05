018fafc8c46f7c84efb960d11730321789002ef3
ABuljko <ajdin.buljko17@gmail.com>
2025-05-05 18:58:23 +0200

    ss

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ let weather = {
       "Wind speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      "url('https://picsum.photos/1600/900?random=" + name + "')";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
